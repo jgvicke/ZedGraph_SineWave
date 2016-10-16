1c9610a97a165c0371439b75962d60eebca5bd7f
John Vickers <jgvicke@gmail.com>
2016-10-15 22:58:12 -0400

    Initial Release

---
+++ MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SineWave
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.generateData = new System.Windows.Forms.Timer(this.components);
+            this.graphUpdate = new System.Windows.Forms.Timer(this.components);
+            this.resumeScrolling = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.zedGraphControl1.Location = new System.Drawing.Point(10, 11);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(425, 295);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.ScrollEvent += new System.Windows.Forms.ScrollEventHandler(this.zedGraphControl1_ScrollEvent);
+            // 
+            // generateData
+            // 
+            this.generateData.Enabled = true;
+            this.generateData.Interval = 10;
+            this.generateData.Tick += new System.EventHandler(this.generateData_Tick);
+            // 
+            // graphUpdate
+            // 
+            this.graphUpdate.Enabled = true;
+            this.graphUpdate.Tick += new System.EventHandler(this.graphUpdate_Tick);
+            // 
+            // resumeScrolling
+            // 
+            this.resumeScrolling.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resumeScrolling.Location = new System.Drawing.Point(9, 319);
+            this.resumeScrolling.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.resumeScrolling.Name = "resumeScrolling";
+            this.resumeScrolling.Size = new System.Drawing.Size(426, 30);
+            this.resumeScrolling.TabIndex = 1;
+            this.resumeScrolling.Text = "Resume Scrolling";
+            this.resumeScrolling.UseVisualStyleBackColor = true;
+            this.resumeScrolling.Click += new System.EventHandler(this.resumeScrolling_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 359);
+            this.Controls.Add(this.resumeScrolling);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MainForm";
+            this.Text = "SineWave by John Vickers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Timer generateData;
+        private System.Windows.Forms.Timer graphUpdate;
+        private System.Windows.Forms.Button resumeScrolling;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace SineWave
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private GraphPane _myPane;
+        private RollingPointPairList _points;
+
+        private double _sinPart;
+        private double _timePart;
+        private XDate _currentTime;
+        private int _i;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _currentTime = new XDate(DateTime.Now);
+            _timePart = 1 / 5000.0;
+            _sinPart = 2 * Math.PI / 5000.0;
+            _i = 0;
+
+            zedGraphControl1.IsShowHScrollBar = true;
+            zedGraphControl1.IsAutoScrollRange = true;
+
+            _myPane = zedGraphControl1.GraphPane;
+            _myPane.Title.Text = "Live Data";
+            _myPane.XAxis.Title.Text = "Time";
+            _myPane.YAxis.Title.Text = "Value";
+
+
+            _myPane.YAxis.Scale.Min = -1.2;
+            _myPane.YAxis.Scale.Max = 1.2;
+            _myPane.YAxis.Scale.MajorStep = 1;
+            _myPane.YAxis.Scale.MinorStep = 1;
+
+            _myPane.XAxis.Type = AxisType.Date;
+            _myPane.XAxis.Scale.Format = "mm:ss:fff";
+            _myPane.XAxis.Scale.FontSpec.Angle = 60;
+            _myPane.XAxis.Scale.FontSpec.Size = 12;
+            _myPane.XAxis.Scale.MajorUnit = DateUnit.Millisecond;
+            _myPane.XAxis.Scale.MajorStep = 500;
+            _myPane.XAxis.Scale.MinorUnit = DateUnit.Millisecond;
+            _myPane.XAxis.Scale.MinorStep = 250;
+
+            _points = new RollingPointPairList(15000);
+            _myPane.AddCurve("Sine Wave", _points, Color.Blue, SymbolType.None);
+
+        }
+
+        private void generateData_Tick(object sender, EventArgs e)
+        {
+
+            for (var j = 0; j < 50; j++)
+            {
+                _i++;
+                _currentTime.AddSeconds(_timePart);
+                _points.Add(_currentTime, Math.Sin(_sinPart * _i));
+            }
+
+            if (_i >= 15000)
+            {
+                //generateData.Enabled = false;
+            }
+
+        }
+
+        private void graphUpdate_Tick(object sender, EventArgs e)
+        {
+            if (!_scrolling)
+            {
+                _myPane.XAxis.Scale.Max = new XDate(_currentTime.DateTime.AddMilliseconds(0));
+                _myPane.XAxis.Scale.Min = new XDate(_currentTime.DateTime.AddMilliseconds(-1000));
+            }
+            _myPane.XAxis.Scale.BaseTic = new XDate(new XDate(_myPane.XAxis.Scale.Min).DateTime.Floor(new TimeSpan(0, 0, 0, 0, (int)_myPane.XAxis.Scale.MajorStep)));
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private bool _scrolling;
+
+        private void zedGraphControl1_ScrollEvent(object sender, ScrollEventArgs e)
+        {
+            _scrolling = true; 
+        }
+
+        private void resumeScrolling_Click(object sender, EventArgs e)
+        {
+            _scrolling = false; 
+        }
+
+    }
+
+    public static class DateExtensions
+    {
+        public static DateTime Round(this DateTime date, TimeSpan span)
+        {
+            var ticks = (date.Ticks + span.Ticks / 2 + 1) / span.Ticks;
+            return new DateTime(ticks * span.Ticks);
+        }
+        public static DateTime Floor(this DateTime date, TimeSpan span)
+        {
+            var ticks = date.Ticks / span.Ticks;
+            return new DateTime(ticks * span.Ticks);
+        }
+        public static DateTime Ceil(this DateTime date, TimeSpan span)
+        {
+            var ticks = (date.Ticks + span.Ticks - 1) / span.Ticks;
+            return new DateTime(ticks * span.Ticks);
+        }
+    }
+
+}
